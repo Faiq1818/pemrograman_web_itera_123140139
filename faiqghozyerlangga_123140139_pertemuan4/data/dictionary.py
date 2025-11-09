aba54f5a054758dc1e9a93b322c7244fdeec8e36
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-11-09 21:08:01 +0700

    add screenshots

--- faiqghozyerlangga_123140139_pertemuan4/data/dictionary.py
+++ faiqghozyerlangga_123140139_pertemuan4/data/dictionary.py
@@ -1,37 +1,37 @@
 mahasiswa = [
     {
-      "nama": "A",
+      "nama": "Lort Sina El Melloi",
       "nim": "123123001",
       "nilai_uts": 50,
       "nilai_uas": 50,
       "nilai_tugas": 30,
     },
     {
-      "nama": "B",
+      "nama": "Fqwawa",
       "nim": "123123002",
-      "nilai_uts": 70,
-      "nilai_uas": 50,
-      "nilai_tugas": 40,
+      "nilai_uts": 90,
+      "nilai_uas": 100,
+      "nilai_tugas": 80,
     },
     {
-      "nama": "C",
+      "nama": "Barrel Titor",
       "nim": "123123003",
       "nilai_uts": 80,
-      "nilai_uas": 20,
+      "nilai_uas": 90,
       "nilai_tugas": 50,
     },
     {
-      "nama": "D",
+      "nama": "John Doe",
       "nim": "123123004",
       "nilai_uts": 70,
       "nilai_uas": 50,
       "nilai_tugas": 40,
     },
     {
-      "nama": "E",
+      "nama": "Hououin",
       "nim": "123123005",
       "nilai_uts": 60,
       "nilai_uas": 70,
-      "nilai_tugas": 50,
+      "nilai_tugas": 80,
     }
 ]
