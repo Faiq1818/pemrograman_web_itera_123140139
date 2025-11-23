e6e82cc888b2ef833fc0a45c297a0d1db99df526
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-11-23 20:48:46 +0700

    add some route

--- faiqghozyerlangga_123140139_pertemuan6/pyramid_scaffold/pyramid_scaffold/views/default.py
+++ faiqghozyerlangga_123140139_pertemuan6/pyramid_scaffold/pyramid_scaffold/views/default.py
@@ -1,8 +1,52 @@
 from pyramid.view import view_config
 from pyramid.response import Response
 from sqlalchemy.exc import SQLAlchemyError
+from pyramid.httpexceptions import (
+    HTTPBadRequest,
+)
 
 from .. import models
+from ..models import Matakuliah 
+
+@view_config(route_name='matakuliah_list', renderer='json')
+def mahasiswa_list(request):
+    """View untuk menampilkan daftar mahasiswa"""
+    dbsession = request.dbsession
+    mahasiswas = dbsession.query(Matakuliah).all()
+    return {'mahasiswas': mahasiswas}
+
+@view_config(route_name='matakuliah_add', request_method='POST', renderer='json')
+def mahasiswa_add(request):
+    """View untuk menambahkan mahasiswa baru"""
+    try:
+        # Ambil data dari request JSON
+        json_data = request.json_body
+
+        # Validasi data minimal
+        required_fields = ['kode_mk', 'nama_mk', 'sks', 'semester']
+        for field in required_fields:
+            if field not in json_data:
+                return HTTPBadRequest(
+                    json_body={'error': f'Field {field} wajib diisi'}
+                )
+
+        # Buat objek Mahasiswa baru
+        matakuliah = Matakuliah(
+            kode_mk=json_data['kode_mk'],
+            nama_mk=json_data['nama_mk'],
+            sks=json_data['sks'],
+            semester=json_data['semester']
+        )
+
+        # Simpan ke database
+        dbsession = request.dbsession
+        dbsession.add(matakuliah)
+        dbsession.flush()  # Untuk mendapatkan ID yang baru dibuat
+
+        return {'success': True, 'matakuliah': matakuliah.to_dict()}
+
+    except Exception as e:
+        return HTTPBadRequest(json_body={'error': str(e)})
 
 
 @view_config(route_name='home', renderer='pyramid_scaffold:templates/mytemplate.jinja2')
